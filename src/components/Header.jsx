c2ad57e5fee92e4a3cd7ad33fc5c9c2bdca07594
adamheincz <heinczadam@gmail.com>
2025-03-04 17:47:37 +0100

    Implemented Header component.

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+export default function Header() {
+    return (
+        <header className="w-full flex items-center justify-between p-4 mt-5">
+            <img className="size-[28px] mx-3" src="src/assets/sonnendent_logo.png" alt="Sonnendent logo"/>
+            <nav className="font-semibold text-2xl text-sd-blue">
+                <ul className="flex gap-8">
+                    <li>
+                        <a href="">Rólunk</a>
+                    </li>
+                    <li>
+                        <a href="">Kezelések</a>
+                    </li>
+                    <li>
+                        <a href="">Elérhetőség</a>
+                    </li>
+                </ul>
+            </nav>
+            <div className="font-semibold text-2xl">
+                <button className="text-sd-blue py-3 px-6 rounded-4xl">Bejelentkezés</button>
+                <button className="text-white bg-sd-orange py-3 px-6 rounded-4xl">Időpontfoglalás</button>
+            </div>
+        </header>
+    );
+}
